4de8abd95f11efec6a9fe407f5cdaa8dfc92f418
markopolo139 <marekseget@onet.pl>
2021-09-03 19:00:44 +0200

    feat(schema): compleate app database schema

--- App/src/main/java/com/chatter/chatter/AppApplication.java
+++ App/src/main/java/com/chatter/chatter/AppApplication.java
@@ -2,6 +2,28 @@ package com.chatter.chatter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//TODO:
+// Security:
+//		Login (1)
+//		Registration (Profile photo) (1)
+//		Forgot Password (recovery) (1)
+//		(Future) Security for message, users in base and sending files
+//		Maybe use JWT (tokens)
+// Base:
+//		Storing user login and password (used to login) (1)
+//		User Friends (1)
+//		User Pending friend request (1)
+//		Groups (?) (Group name and group admin as PK)
+//	WebSocket:
+//		Messaging (to user and groups)
+//		Sending files (etc PDF, photos) (?)
+//	Application:
+//		setting Profile photo (1) (check if photo use content type)
+//		Sending files (etc PDF, photos)
+//		Friends Requests (add or decline) (if 'add' add user to user friends and reverse)(later delete pending request) (1)
+//		Search for friend (1)
+//		Creating groups (1/?)
+//		Creating message (1)
 
 @SpringBootApplication
 public class AppApplication {
--- schema/db-base.sql
+++ schema/db-base.sql
@@ -1,10 +1,38 @@
 create table user_info(
-                          user_id int  not null primary key auto_increment,
+                          user_id int not null primary key auto_increment,
                           login varchar(100) not null,
                           password varchar(150) not null,
                           email varchar(100) not null,
                           password_token varchar(100) null,
-                          enabled boolean not null
+                          enabled boolean not null,
+                          photo MEDIUMBLOB
+);
+
+create table user_friends(
+                          user_id int not null,
+                          friend_id int not null,
+                          constraint user_to_user foreign key (user_id) references user_info (user_id),
+                          constraint friend_to_user foreign key (friend_id) references user_info (user_id)
+);
+
+create table user_friends_request(
+                          user_id int not null,
+                          friend_id int not null,
+                          constraint user_to_user foreign key (user_id) references user_info (user_id),
+                          constraint friend_to_user foreign key (friend_id) references user_info (user_id)
+);
+
+create table user_groups(
+                            group_id int not null primary key auto_increment,
+                            group_name String not null,
+                            admin_id int not null,
+                            constraint user_to_user foreign key (admin_id) references user_info (user_id),
+);
+
+create table group_users(
+                          group_id int not null,
+                          friend_id int not null,
+                          constraint friend_to_user foreign key (friend_id) references user_groups (group_id)
 );
 
 create table user_roles(
