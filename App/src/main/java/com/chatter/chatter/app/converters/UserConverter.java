0776f7e0fcd15478134a9247a937d49c96b3a8fd
markopolo139 <marekseget@onet.pl>
2021-09-17 16:36:13 +0200

    refactor: above

--- App/src/main/java/com/chatter/chatter/app/converters/UserConverter.java
+++ App/src/main/java/com/chatter/chatter/app/converters/UserConverter.java
@@ -3,17 +3,16 @@ package com.chatter.chatter.app.converters;
 import com.chatter.chatter.app.data.entity.UserEntity;
 import com.chatter.chatter.web.models.response.UserModel;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
 public interface UserConverter {
 
-    UserModel UserEntityToUserProfile(UserEntity userEntity) throws IOException;
+    UserModel UserEntityToUserProfile(UserEntity userEntity);
 
-    List<UserModel> UserEntityListToModelList(Set<UserEntity> userEntityList) throws IOException;
+    List<UserModel> UserEntityListToModelList(Set<UserEntity> userEntityList);
 
-    List<UserModel> FriendsToUserModelList(UserEntity userEntity) throws IOException;
+    List<UserModel> FriendsToUserModelList(UserEntity userEntity);
 
-    List<UserModel> RequestsToUserModelList(UserEntity userEntity) throws IOException;
+    List<UserModel> RequestsToUserModelList(UserEntity userEntity);
 }
