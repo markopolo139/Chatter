b389742eebfc7579233621d7ef8675e7646369d5
markopolo139 <marekseget@onet.pl>
2021-10-16 21:53:05 +0200

    feat(userMessageRepository): create query for getting messages

--- App/src/main/java/com/chatter/chatter/app/data/repository/GroupRepository.java
+++ App/src/main/java/com/chatter/chatter/app/data/repository/GroupRepository.java
@@ -20,7 +20,8 @@ public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
 
     @Query(value = "select * from user_groups ug" +
             " where ug.admin_id = (select user_id from app_users where app_users.login = :login) " +
-            "or ug.group_id in (select uig.group_id from app_users au join users_in_group uig on au.user_id = uig.friend_id where login = :login)", nativeQuery = true)
+            "or ug.group_id in (select uig.group_id from app_users au join users_in_group uig " +
+            "on au.user_id = uig.friend_id where login = :login)", nativeQuery = true)
     List<GroupEntity> findAllGroupsWithLogin(@Param("login") String login);
 
     @Transactional
--- App/src/main/java/com/chatter/chatter/app/data/repository/UserMessageRepository.java
+++ App/src/main/java/com/chatter/chatter/app/data/repository/UserMessageRepository.java
@@ -3,6 +3,8 @@ package com.chatter.chatter.app.data.repository;
 import com.chatter.chatter.app.data.entity.UserMessagesEntity;
 import com.chatter.core.values.MessageStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,6 +13,15 @@ public interface UserMessageRepository extends JpaRepository<UserMessagesEntity,
     List<UserMessagesEntity> findAllByUserToId_LoginAndUserFromId_Login(String userToLogin, String userFromLogin);
 
     List<UserMessagesEntity> findAllByUserToId_LoginAndUserFromId_LoginAndStatus(
-            String userToLogin, String userFromLogin, MessageStatus status);
+            String userToLogin, String userFromLogin, MessageStatus status
+    );
+
+    @Query(
+            "select use from UserMessagesEntity use where use.userFromId.login in (:loggedInLogin, :chatPersonLogin)" +
+            " and use.userToId.login in(:loggedInLogin, :chatPersonLogin) and use.userFromId.login <> use.userToId.login "
+    )
+    List<UserMessagesEntity> findAllMessagesBetweenLoggedInAndChatPerson(
+            @Param("loggedInLogin") String loggedInLogin, @Param("chatPersonLogin") String chatPersonLogin
+    );
 
 }
